58a97a617a5f6a34baaa59a65d39a9c46fbfdd1e
luckychdl <luckysksk@naver.com>
2021-10-21 05:57:19 +0900

    Update: Hosting

--- src/commons/firebase/firebase.ts
+++ src/commons/firebase/firebase.ts
@@ -2,14 +2,14 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-// 프로젝트 공용 (JH)
 const clientCredentials = {
-  apiKey: "AIzaSyDUode-3Ahyy5698hB0usCiM23KRpW2gNo",
-  authDomain: "pensooz-fire.firebaseapp.com",
-  projectId: "pensooz-fire",
-  storageBucket: "pensooz-fire.appspot.com",
-  messagingSenderId: "1080768406589",
-  appId: "1:1080768406589:web:9c7768048dc368a4c1f654",
+  apiKey: "AIzaSyDnIUm_X60oLLTOnRHSdwiFIjvi2btG7zc",
+  authDomain: "codecamp-446bd.firebaseapp.com",
+  projectId: "codecamp-446bd",
+  storageBucket: "codecamp-446bd.appspot.com",
+  messagingSenderId: "910012870523",
+  appId: "1:910012870523:web:7dafe14a72550b92e657de",
+  measurementId: "G-LMFVJRMQWN",
 };
 
 if (!firebase.apps.length) {
